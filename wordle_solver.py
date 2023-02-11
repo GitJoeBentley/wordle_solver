6f64928c64a909df0ec233340d9eefb827a03389
Joe <joe.deanza@gmail.com>
2023-02-11 04:37:41 -0800

    Minor fixes to the code and handling file not open exception

--- wordle_solver.py
+++ wordle_solver.py
@@ -32,7 +32,6 @@ def changeLetterToSpaceInWord(word,pos):
 def evaluate_guess(guess, words, the_word):
     guess_list = list(guess)
     the_word_list = list(the_word)
-    #word_copy = the_word
     evaluation = [0,0,0,0,0]
     i = 0
     # Look for # 2 = right letter, right place
@@ -64,7 +63,6 @@ def wordMatchesPattern(guess, pattern, words):
     temp_words = []
     
     # Check pattern string for 0s
-    exclude = ""
     for i in range(0,5):
         if pattern[i] == 0:
             for word in words:
@@ -73,7 +71,6 @@ def wordMatchesPattern(guess, pattern, words):
         if len(temp_words) > 0:
             words = temp_words
             temp_words = []
-    #print("62:",i," len(words)=",len(words))
     if guess == "stave":
         print("63:",words)
                                     
@@ -87,7 +84,6 @@ def wordMatchesPattern(guess, pattern, words):
         if len(temp_words) > 0:
             words = temp_words
             temp_words = []
-    #print("74:",i," len(words)=",len(words))
                     	
     # Check pattern string for 1s
     for i in range(0,5):
@@ -100,46 +96,48 @@ def wordMatchesPattern(guess, pattern, words):
             words = temp_words
             temp_words = []                    	
     return words
-    
+
+ 
 wordfile = "/media/joe/E_drive/software/sfml/wordle/resources/wordfile.txt"
 words = []
 counter = 0
 temp_words = []
-
-with open(wordfile) as fin:
-    for word in fin:
-        words.append(word.rstrip())
-    fin.close()
-        
-the_word = getRandomWord(words)
-#the_word = "error"
-the_word = "stage"
 game_over = False
-
 guesses = 0
-while not game_over:
-    guesses += 1
-    if (guesses > 6):
-        print("Too bad!  You lose.")
-        break
-    guess = getRandomWord(words)
-    #if (guesses == 1):
-    #    guess = "earth"
-    print("Guess #",guesses,"the_word=",the_word,"  guess=",guess)
-    if (guess == the_word):
-        game_over = True
-        print("You win!!!  And it only took you",guesses,"guesses.")
-        break
-        		
-    pattern = evaluate_guess(guess,words,the_word)
-    print("Evaluation:",pattern)
-    words = wordMatchesPattern(guess,pattern,words)
-    if guess in words:
-        words.remove(guess)
-    print("Number of words left to choose from",len(words))
-    if (len(words) < 34):
-        print(words)
-    print("")
-        	
-        	
-        	
+
+try:
+    with open(wordfile) as fin:
+        for word in fin:
+            words.append(word.rstrip())
+        fin.close()
+            
+    the_word = getRandomWord(words)
+    #the_word = "error"
+    the_word = "stage"
+
+    while not game_over:
+        guesses += 1
+        if (guesses > 6):
+            print("Too bad!  You lose.")
+            break
+        guess = getRandomWord(words)
+        #if (guesses == 1):
+        #    guess = "earth"
+        print("Guess #",guesses,"the_word=",the_word,"  guess=",guess)
+        if (guess == the_word):
+            game_over = True
+            print("You win!!!  And it only took you",guesses,"guesses.")
+            break
+                    
+        pattern = evaluate_guess(guess,words,the_word)
+        print("Evaluation:",pattern)
+        words = wordMatchesPattern(guess,pattern,words)
+        if guess in words:
+            words.remove(guess)
+        print("Number of words left to choose from",len(words))
+        if (len(words) < 34):
+            print(words)
+        print("")
+
+except Exception as e:
+    print("Unable to open file",wordfile)
